458e7d3bcfe65c684bcb6ff64407cc880930837e
Jakub Grill <jakubgri@onet.pl>
2022-03-21 02:45:51 +0100

    Missing file modification in task 3

--- app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ app/src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -2,16 +2,14 @@ package pl.futurecollars.invoicing.service;
 
 import java.util.List;
 import java.util.Optional;
+import lombok.AllArgsConstructor;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
+@AllArgsConstructor
 public class InvoiceService {
 
   private final Database database;
-
-  public InvoiceService(Database database) {
-    this.database = database;
-  }
 
   public int save(Invoice invoice) {
     return database.save(invoice);
